dbb1969153e43b6729a9c346ad6cfc6422e6e410
Syed Mir Hamza <Syed Mir Hamza@DESKTOP-EB8H1AS>
2022-12-13 16:59:36 +0500

    added lines for I2C communication with the eeprom

--- lcd_program/Core/Inc/eeprom_chip.h
+++ lcd_program/Core/Inc/eeprom_chip.h
@@ -9,6 +9,10 @@
 #define INC_EEPROM_CHIP_H_
 #include "main.h"
 #include "SW3516.h"
+
+
+#define eeprom_address 0xA0
+
 typedef enum {
 
 	max_voltage=1,
@@ -42,7 +46,7 @@ extern  eeprom_data max_eeprom_data;
 extern  set_eeprom set_eeprom_data;
 
 
-void write_eeprom(uint16_t *data,parameter_type type);
+void write_eeprom(uint16_t data,parameter_type type);
 void read_eeprom(parameter_type type);
 
 #endif /* INC_EEPROM_CHIP_H_ */
