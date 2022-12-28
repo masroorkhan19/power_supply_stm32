31f8d244a4bc03c5c88fab7777a39e07c198e9f9
Syed Mir Hamza <Syed Mir Hamza@DESKTOP-EB8H1AS>
2022-12-28 21:08:57 +0500

    lcd program updated to include page 4 tab2 and tab3

---
+++ lcd_program/Core/Inc/buzzer.h
@@ -0,0 +1,15 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Dec 25, 2022
+ *      Author: Syed Mir Hamza
+ */
+
+#ifndef INC_BUZZER_H_
+#define INC_BUZZER_H_
+
+#include "main.h"
+
+void Beep(void);
+
+#endif /* INC_BUZZER_H_ */
